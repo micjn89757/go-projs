8b6b40d32124df5eda3d3a49012bfccfaa716d09
djn <1980846456@qq.com>
2024-05-28 15:44:48 +0800

    add main

---
+++ semaphore-gin/main.go
@@ -0,0 +1,26 @@
+package semaphoregin
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(ctx *gin.Context) {
+		// Call the HTML method of the Context to render a template
+		ctx.HTML(
+			// Set the HTTP status to 200 (OK)
+			http.StatusOK,
+			// Use the index.html template
+			"index.html",
+			// Pass the data that the page uses (in this case, 'title')
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+	})
+}
