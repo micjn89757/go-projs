e6688f69bca356b302c53f97541d228d28b69bc2
djn <1980846456@qq.com>
2024-06-08 17:27:00 +0800

    complete jwt

--- vote-gin/middleware/jwt.go
+++ vote-gin/middleware/jwt.go
@@ -1,10 +1,19 @@
 package middleware
 
 import (
+	"fmt"
+	"time"
 	"vote-gin/utils"
 
 	"github.com/golang-jwt/jwt/v5"
+	"go.uber.org/zap"
 )
+
+var sugar *zap.SugaredLogger
+
+func init() {
+	sugar = utils.Logger.Sugar()
+}
 
 type JWT struct {
 	JwtKey []byte
@@ -19,4 +28,29 @@ func NewJWT() *JWT {
 type MyClaims struct {
 	Username string `json:"username"`
 	jwt.RegisteredClaims
+}
+
+
+// CreateToken 创建token
+func (j *JWT) CreateToken(claim MyClaims)(string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claim)
+	return token.SignedString(j.JwtKey)
+}
+
+// ParseToken 解析token 字符串
+func (j *JWT) ParseToken(tokenString string) error {
+	token, err := jwt.ParseWithClaims(tokenString, &MyClaims{}, func(token *jwt.Token)(any ,error) {
+		return j.JwtKey, nil
+	}, jwt.WithLeeway(5*time.Second))
+
+	if err != nil {
+		NewErr := fmt.Errorf("parse jwt string failed: %w", err)
+		sugar.Error(NewErr.Error())
+		return NewErr
+	} else if _, ok := token.Claims.(*MyClaims); ok && token.Valid {
+		return nil
+	} else {
+		sugar.Error(jwt.ErrInvalidKeyType.Error())
+		return jwt.ErrInvalidType
+	}
 }
