e239cc92e915c558589eaed7fe32094bbe2e45ae
djn <1980846456@qq.com>
2024-06-18 23:07:15 +0800

    model complete

--- vote-gin/model/vote_opt_user.go
+++ vote-gin/model/vote_opt_user.go
@@ -5,7 +5,7 @@ type VoteOptUser struct {
 	Base
 	UserID	int `db:"user_id" json:"user_id"`
 	VoteID 	int `db:"vote_id" json:"vote_id"`
-	VoteOptID int `db:"vote_opt_id" json"vote_opt_id"`
+	VoteOptID int `db:"vote_opt_id" json:"vote_opt_id"`
 }
 
 
