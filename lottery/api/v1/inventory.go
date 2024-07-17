eb5147a7af3538eaff50136356e456baa483cba5
djn <1980846456@qq.com>
2024-07-17 23:16:57 +0800

    update redis

--- lottery/api/v1/inventory.go
+++ lottery/api/v1/inventory.go
@@ -66,8 +66,9 @@ func Lottery(ctx *gin.Context) {
 		// !用户ID写死1
 		model.PutOrder(1, invId) // 订单信息写入channel
 
+		ctx.String(http.StatusOK, strconv.Itoa(int(invId)))
 		// 抽奖算法是个概率问题，但是10次都不成功是小概率时间，但是不能为了特别小的概率时间做太多处理
+		return 
 	}
-
-	ctx.String(http.StatusOK, strconv.Itoa(int(invId)))
+	ctx.String(http.StatusOK, strconv.Itoa(1)) //如果10次之后还失败，则返回“谢谢参与”
 }
